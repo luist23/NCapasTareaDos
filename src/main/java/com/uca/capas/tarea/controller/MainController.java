bea25182427ca7c8418bc21a579b7274806a640b
luist23 <00127717@uca.edu.sv>
2020-04-20 20:32:00 -0700

    add parte uno  tarea

--- src/main/java/com/uca/capas/tarea/controller/MainController.java
+++ src/main/java/com/uca/capas/tarea/controller/MainController.java
@@ -11,36 +11,45 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-	@RequestMapping("/ejemplo")
+	@RequestMapping("/alumno")
 	public @ResponseBody String ejemplo() {
-		String texto = "Hola Mundo desde Spring MVC! La hora actual es: ";
-		return texto.concat(Calendar.getInstance().getTime().toString());
+		return 	"Nombre: Luis Adalberto" + "\n" +
+				"Apellido: Trujillo Perez" + "\n" +
+				"Carnet: 00127717" + "\n"+ 
+				"Carrera: Ingeniería Informática" + "\n"+
+				"Año Cursado: 3°" + "\n";
+		
 	}
 
 	@RequestMapping("/parametro")
-	public @ResponseBody String parametro(HttpServletRequest request) {
+	public @ResponseBody String parametro(HttpServletRequest request, HttpServletRequest request2, HttpServletRequest request3) {
 		Integer param = Integer.parseInt(request.getParameter("dia"));
-		String dia;
-		switch (param) {
-			case 1: dia = "Lunes";
-			break;
-			case 2: dia = "Martes";
-			break;
-			case 3: dia = "Miercoles";
-			break;
-			case 4: dia = "Jueves";
-			break;
-			case 5: dia = "Viernes";
-			break;
-			case 6: dia = "Sabado";
-			break;
-			case 7: dia = "Domingo";
-			break;
-			default: dia = "Ningun dia seleccionado";
-			break;
-		}
+		Integer param2 = Integer.parseInt(request.getParameter("mes"));
+		Integer param3 = Integer.parseInt(request.getParameter("año"));
 		
-		return "Dia de la semana enviado: " + dia;
+		Calendar c = Calendar.getInstance();
+		c.set(param3,param2,param);
+		int intDia = c.get(Calendar.DAY_OF_WEEK);
+		String dia;
+		switch(intDia) {
+		case 2: dia="Lunes";
+		break;
+		case 3: dia="Martes";
+		break;
+		case 4: dia="Miercoles";
+		break;
+		case 5: dia="Jueves";
+		break;
+		case 6: dia="Viernes";
+		break;
+		case 7: dia="Sabado";
+		break;
+		case 1: dia="Domingo";
+		break;
+		default: dia="Ningun día seleccionado";
+		break;
+		}
+		return "El dia de la semana es   "+dia;
 	}
-
+	
 }
